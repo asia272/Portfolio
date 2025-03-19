d09a5cd6979ceb38bdc3907991b893dafaf9a10e
Asia Ashraf <asiaashraf7272@gmail.com>
2025-03-19 14:50:58 +0500

    animation added

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -3,7 +3,7 @@ import "../HeroSection/HeroSection.css";
 import myImg from "../../assets/myimg.jpeg";
 import { Typewriter } from "react-simple-typewriter";
 import BackHandIcon from "@mui/icons-material/BackHand";
-import { motion } from "motion/react";
+import { motion } from "framer-motion";
 
 const HeroSection = () => {
   return (
@@ -11,9 +11,22 @@ const HeroSection = () => {
       <div className="container">
         <div className="content">
           <i className="boi-i">Web Developer,</i>
-          <h1 className="name">Asia Ashraf</h1>
-          <h1 className="type">
-            I'm a <span>
+          <motion.h1
+            className="name"
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8, ease: "easeOut" }}
+          >
+            Asia Ashraf
+          </motion.h1>
+          <motion.h1
+            className="type"
+            initial={{ opacity: 0, scale: 0.8 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 1, ease: "easeOut", delay: 0.5 }}
+          >
+            I'm a{" "}
+            <span>
               <Typewriter
                 words={["Web Developer", "Frontend Developer"]}
                 loop={true}
@@ -24,18 +37,39 @@ const HeroSection = () => {
                 delaySpeed={1500}
               />
             </span>
-          </h1>
-          <p>
+          </motion.h1>
+
+          <motion.p
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1, ease: "easeOut", delay: 1 }}
+          >
             As a dedicated front-end developer, I specialize in designing and
             building dynamic, interactive user interfaces that deliver seamless
             and engaging experiences.
-          </p>
-          <a href="#contact" className="hi a-btn">
+          </motion.p>
+
+    
+          <motion.a
+            href="#contact"
+            className="hi a-btn"
+            initial={{ scale: 0.8, opacity: 0 }}
+            animate={{ scale: 1, opacity: 1 }}
+            transition={{ duration: 0.5, delay: 1.5 }}
+          >
             Say Hi,
             <BackHandIcon className="hi hand-icon" />
-          </a>
+          </motion.a>
         </div>
-        <img src={myImg} alt="Asia Ashraf" className="img-box" />
+
+        <motion.img
+          src={myImg}
+          alt="Asia Ashraf"
+          className="img-box"
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 1, ease: "easeOut", delay: 1 }}
+        />
       </div>
     </div>
   );
