5ea19f4f5db2966f846d7cf1125c6ecb8b70f3ba
Asia Ashraf <asiaashraf7272@gmail.com>
2025-03-29 18:32:06 +0500

    update layout

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -62,14 +62,14 @@ const HeroSection = () => {
           </motion.a>
         </div>
 
-        <motion.img
-          src={myImg}
+        <div className="img-box">
+         <motion.img src={myImg}
           alt="Asia Ashraf"
-          className="img-box"
           initial={{ opacity: 0, scale: 0.5 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 1, ease: "easeOut", delay: 1 }}
         />
+        </div>
       </div>
     </div>
   );
