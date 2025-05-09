42ae0d5950cf059cda4a7e1e87a1d0fe5c3cc25b
Asia Ashraf <asiaashraf7272@gmail.com>
2025-05-09 18:39:03 +0500

    spin cart animation added

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -1,25 +1,26 @@
 import React from 'react'
 import { FaLongArrowAltRight } from "react-icons/fa";
 
-const ProjectCard = ({projects}) => {
+const ProjectCard = ({ projects }) => {
   return (
     <div className='card-container'>
       {projects.map((project, index) => (
-          <div key={index} className='cardBox'>
+        <div key={index} className='cardBox'>
+          <div className='card-content-wrapper'>
             <img src={project.img} alt="img" />
-              <div className="cardContent">
-                <h3>{project.name}</h3>
-                <a href={project.link}>
+            <div className="cardContent">
+              <h3>{project.name}</h3>
+              <a href={project.link}>
                 Open Project
-                  <FaLongArrowAltRight className='arrowIcon' />
-                  </a>
-                  {project.site && 
-                  <a href={project.site}>Live Demo</a>
-                   }
-              </div>
-
+                <FaLongArrowAltRight className='arrowIcon' />
+              </a>
+              {project.site && 
+                <a href={project.site}>Live Demo</a>
+              }
+            </div>
           </div>
-        ))}
+        </div>
+      ))}
     </div>
   )
 }
